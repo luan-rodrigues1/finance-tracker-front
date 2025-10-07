9fcbb56d10618d450b76562239e2aa5fbab358d5
luan-rodrigues1 <rodrigues.luan20@gmail.com>
2025-10-07 11:08:52 -0300

    feat: add total balance calculation

--- src/app/components/OverviewCard/index.tsx
+++ src/app/components/OverviewCard/index.tsx
@@ -1,4 +1,20 @@
+import { useTransactions } from "@/app/Context/TransactionContext";
+
 const OverviewCard = () => {
+    const {transactions} = useTransactions();
+
+    const totalBalance = transactions.reduce((acc, transaction) => {
+        return transaction.isIncome
+        ? acc + transaction.amount
+        : acc - transaction.amount;
+    }, 0);
+
+    const formattedBalance = totalBalance.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+
+
     return (
         <div className="min-h-24 border-1 rounded-sm border-grey-2 p-4 flex justify-between">
             <div className="flex flex-col gap-3 text-grey-4">
@@ -6,7 +22,7 @@ const OverviewCard = () => {
                 <p className="text-body font-normal">O valor se refere ao saldo</p>
             </div>
             <div className="text-primary font-bold">
-                <p>R$ 8184,00</p>
+                <p>{formattedBalance}</p>
             </div>
         </div>
     )
